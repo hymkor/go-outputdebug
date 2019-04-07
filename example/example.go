0884c64147603dfa6a3e06dfee9b23a764be573d
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-04-08 01:32:24 +0900

    Add variable `Out` supporting io.Writer interface

--- example/example.go
+++ example/example.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	".."
+	"fmt"
+	"github.com/zetamatta/go-outputdebug"
 )
 
+var dbg = outputdebug.Out
+
 func main() {
-	outputdebug.String("example.go: main() Call dbg.Print")
+	outputdebug.String("outputdebug.String()")
+	fmt.Fprintf(dbg, "example.go: main() Call dbg.Print\n")
+	fmt.Fprintf(dbg, "foo\nbar\ndddddd\n")
 }
--- string.go
+++ string.go
@@ -1,6 +1,7 @@
 package outputdebug
 
 import (
+	"bytes"
 	"syscall"
 	"unsafe"
 )
@@ -14,3 +15,24 @@ func String(s string) {
 		outputDebugStringW.Call(uintptr(unsafe.Pointer(p)))
 	}
 }
+
+type out struct {
+	buffer []byte
+}
+
+func (d *out) Write(b []byte) (int, error) {
+	d.buffer = append(d.buffer, b...)
+	for {
+		eol := bytes.IndexByte(d.buffer, '\n')
+		if eol < 0 {
+			return len(b), nil
+		}
+		String(string(d.buffer[:eol]))
+
+		left := len(d.buffer) - (eol + 1)
+		copy(d.buffer, d.buffer[eol+1:])
+		d.buffer = d.buffer[:left]
+	}
+}
+
+var Out = &out{}
