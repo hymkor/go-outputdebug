1278a2b3f557bc1f1c3426791cf5f8759308b99a
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-05-28 00:17:13 +0900

    Implement unix version which does nothing

--- string.go
+++ string.go
@@ -2,18 +2,10 @@ package outputdebug
 
 import (
 	"bytes"
-	"syscall"
-	"unsafe"
 )
 
-var kernel32 = syscall.NewLazyDLL("kernel32")
-var outputDebugStringW = kernel32.NewProc("OutputDebugStringW")
-
 func String(s string) {
-	p, err := syscall.UTF16PtrFromString(s)
-	if err == nil {
-		outputDebugStringW.Call(uintptr(unsafe.Pointer(p)))
-	}
+	outputdebugstring(s)
 }
 
 type out struct {
---
+++ string_unix.go
@@ -0,0 +1,5 @@
+// +build !windows
+
+package outputdebug
+
+func outputdebugstring(s string) {}
